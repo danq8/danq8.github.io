51f8d24df6547d162d4dd2adcc1f042506070c9e
Dan <dan_q8@hotmail.com>
2024-11-17 12:12:29 +0000

    test2

--- js/customscripts.js
+++ js/customscripts.js
@@ -193,7 +193,7 @@ $(function() {
                     // span.style.lineHeight = '15px';
                     // span.style.overflow = 'hidden';
                     //elmPre[i].appendChild(span);
-                    div.insertBefore(span,elmCod[j].firstChild);
+                    elmCod[j].insertBefore(span,elmCod[j].firstChild);
 
                     // When the Copy button is clicked call the copyToClipboard function and pass it the contents of the <pre>
                     span.onclick = function (e) {
