bec4e82f0a05207ad101a4c36c57cdab92b3ec41
Dan <dan_q8@hotmail.com>
2024-11-17 12:39:46 +0000

    test2

--- js/customscripts.js
+++ js/customscripts.js
@@ -197,7 +197,7 @@ $(function() {
 
                     // When the Copy button is clicked call the copyToClipboard function and pass it the contents of the <pre>
                     span.onclick = function (e) {
-                        copyToClipboard(span.innerText.replace(label, '').trim(), span)
+                        copyToClipboard(elmCod[j].innerText.replace(label, '').trim(), span)
                     }
                 }
             }
